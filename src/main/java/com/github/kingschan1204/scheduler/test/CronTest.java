84b8bf5f31ac2545266e1a0acce0de0032803580
kingschan <kings.chan@qq.com>
2025-03-18 16:24:52 +0800

    add quartz CronExpression

---
+++ src/main/java/com/github/kingschan1204/scheduler/test/CronTest.java
@@ -0,0 +1,19 @@
+package com.github.kingschan1204.scheduler.test;
+
+
+import com.github.kingschan1204.scheduler.core.cron.CronExpression;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class CronTest {
+    public static void main(String[] args) throws Exception {
+        CronExpression cron = new CronExpression("0 30 9 ? * 2-6");
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date date = new Date();
+        for (int i = 0; i < 10; i++) {
+            date = cron.getNextValidTimeAfter(date);
+            System.out.println(sdf.format(date));
+        }
+    }
+}
